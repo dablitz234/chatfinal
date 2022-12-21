7e771a35ddf9e1595dcac7c5aab53c98fd92e568
dablitz234 <dblitz@btinternet.com>
2022-12-21 10:22:23 +0200

    newcommit

--- src/components/ChatMessage/ChatMessage.js
+++ src/components/ChatMessage/ChatMessage.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { UserAuth } from '../../Context/AuthContext';
+import React from "react";
+import { UserAuth } from "../../Context/AuthContext";
 
 const ChatMessage = (props) => {
-    const { text, uid, photoURL } = props.message;
-    const { user } = UserAuth();
-    const messageClass = uid === user.uid ? 'sent' : 'received';
+  const { text, uid, photoURL } = props.message;
+  const { user } = UserAuth();
+  const messageClass = uid === user.uid ? "sent" : "received";
 
-  return (<>
-    <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="profilePic" />
-      <p>{text}</p>
-    </div>
-  </>)
+  return (
+    <>
+      <div className={`message ${messageClass}`}>
+        <img
+          src={
+            photoURL || "https://api.adorable.io/avatars/23/abott@adorable.png"
+          }
+          alt="profilePic"
+        />
+        <p>{text}</p>
+      </div>
+    </>
+  );
 };
 
 export default ChatMessage;
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBRZSgzmMTdHpD_siPGIOgVoK_xTDRhHvU",
+  apiKey: "AIzaSyBRZSgzmMTdHpD_siPGIOgVoK_xTDRhHvU",
   authDomain: "chat-room-fea83.firebaseapp.com",
   projectId: "chat-room-fea83",
   storageBucket: "chat-room-fea83.appspot.com",
   messagingSenderId: "587393897679",
-  appId: "1:587393897679:web:45af73544ff6a2300713ba"
+  appId: "1:587393897679:web:45af73544ff6a2300713ba",
 };
 
 // Initialize Firebase
